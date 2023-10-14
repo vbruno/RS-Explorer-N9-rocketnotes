dfe2ab7082ff8681bf6485fcd1e8d2034782b9c4
Bruno S Velho <bruno.velho@gmail.com>
2023-10-14 16:38:47 -0300

    Aula: Estilo Global

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 
-import { Details } from './pages/Details'
+import GlobalStyles from './styles/global'
+import theme from './styles/theme'
 
-import { theme } from './styles/theme'
+import { Details } from './pages/Details'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Details />
     </ThemeProvider>
   </React.StrictMode>,

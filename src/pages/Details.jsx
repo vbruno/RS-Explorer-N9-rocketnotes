4a6cbcba320b9e43a2d3ee88ac695f65dac5101f
Bruno S Velho <bruno.velho@gmail.com>
2023-10-15 15:51:54 -0300

    Aula: Propriedade

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,9 +1,16 @@
 import { Container } from './styles'
 
+import { Button } from '../components/Button'
+
 export function Details() {
   return (
     <Container>
-      <h1>App</h1>
+      <h1>Hello World</h1>
+      <span>Bruno S Velho</span>
+
+      <Button title='Entrar' loading />
+      <Button title='Cadastrar' />
+      <Button title='Voltar' />
     </Container>
   )
 }

7fa28dce54884d7c76854f2cb0bafbca4d8ad009
Bruno S Velho <bruno.velho@gmail.com>
2023-10-16 10:25:04 -0300

    Aula: Formulário de SignIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,26 @@
-import { Container } from "./styles";
+import { FiMail, FiLock } from 'react-icons/fi'
+
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+import { Container, Form } from "./styles";
 
 export function SignIn() {
   return (
     <Container>
-      <h1>SignIn</h1>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+        <h2>Faça seu login</h2>
+
+        <Input placeholder='E-mail' icon={FiMail} />
+        <Input placeholder='password' icon={FiLock} type="password" />
+
+        <Button title={"Entrar"} />
+
+        <a href="#">Criar conta</a>
+      </Form>
     </Container>
   )
 }

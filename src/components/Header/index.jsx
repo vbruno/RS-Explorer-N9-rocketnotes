1e02bf51ac5b8f2cc99060fe77e89e471eaba6b1
Bruno S Velho <bruno.velho@gmail.com>
2023-10-15 21:50:30 -0300

    Aula: Children

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
-import { Container, Profile } from './styles'
+import { RiShutDownLine } from 'react-icons/ri'
+import { Container, Profile, Logout } from './styles'
 
 export function Header() {
   return (
@@ -15,6 +16,10 @@ export function Header() {
           </strong>
         </div>
       </Profile>
+
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
 
     </Container>
   )

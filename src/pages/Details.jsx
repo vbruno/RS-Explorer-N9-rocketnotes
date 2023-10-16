a4ca60ea02d14ef576867fe1b04ace9595056340
Bruno S Velho <bruno.velho@gmail.com>
2023-10-15 21:07:50 -0300

    Aula: Componente Header

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,16 +1,15 @@
+import { Header } from '../components/Header'
+import { Button } from '../components/Button'
+
 import { Container } from './styles'
 
-import { Button } from '../components/Button'
+
 
 export function Details() {
   return (
     <Container>
-      <h1>Hello World</h1>
-      <span>Bruno S Velho</span>
-
-      <Button title='Entrar' loading />
-      <Button title='Cadastrar' />
-      <Button title='Voltar' />
+      <Header />
+      <Button title={'Voltar'} />
     </Container>
   )
 }

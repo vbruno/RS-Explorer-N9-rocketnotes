71821f85d41932c434208e344be7ddabbb94dff5
Bruno S Velho <bruno.velho@gmail.com>
2023-10-15 22:15:06 -0300

    Aula: Children

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,7 +1,8 @@
 import { Header } from '../components/Header'
 import { Button } from '../components/Button'
+import { Section } from '../components/Section'
 
-import { Container } from './styles'
+import { Container, Links } from './styles'
 
 
 
@@ -9,6 +10,19 @@ export function Details() {
   return (
     <Container>
       <Header />
+
+      <Section title={'Links úteis'}>
+        <Links>
+          <li>
+            <a href="#">https://www.rocketseat.com.br'</a>
+          </li>
+          <li>
+            <a href="#">https://www.rocketseat.com.br'</a>
+          </li>
+
+        </Links>
+      </Section>
+
       <Button title={'Voltar'} />
     </Container>
   )

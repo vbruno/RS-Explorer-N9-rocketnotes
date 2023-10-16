05ccc0a3a88b42a873482863a2011a8858606f24
Bruno S Velho <bruno.velho@gmail.com>
2023-10-16 10:41:18 -0300

    Aula: SignUp

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,30 @@
+import { FiMail, FiLock, FiUser } from 'react-icons/fi'
+
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+import { Container, Form, Background } from "./styles";
+
+export function SignUp() {
+  return (
+    <Container>
+      <Background />
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+        <h2>Crie sua conta</h2>
+
+        <Input placeholder='Nome' icon={FiUser} />
+        <Input placeholder='E-mail' icon={FiMail} />
+        <Input placeholder='password' icon={FiLock} type="password" />
+
+        <Button title={"Cadastrar"} />
+
+        <a href="#">Voltar para o login</a>
+      </Form>
+
+
+    </Container>
+  )
+}

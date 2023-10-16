d854aaaefca04d4d85ac2dee1e4cf956a275c422
Bruno S Velho <bruno.velho@gmail.com>
2023-10-15 21:44:36 -0300

    Aula: Imagem do usuário

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,20 @@
-import { Container } from './styles'
+import { Container, Profile } from './styles'
 
 export function Header() {
   return (
     <Container>
+      <Profile>
+        <img src="http://github.com/vbruno.png" alt="Foto do usuário" />
+
+        <div>
+          <span>
+            Bem-vindo
+          </span>
+          <strong>
+            Bruno S. Velho
+          </strong>
+        </div>
+      </Profile>
 
     </Container>
   )

7cbd716956eef27625d0f59222cabe0c730a95d6
Bruno S Velho <bruno.velho@gmail.com>
2023-10-17 16:49:24 -0300

    Ajuste navegação das rotas

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,11 +3,12 @@ import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
+const authenticated = false;
+
 export function Routes() {
   return (
     <BrowserRouter>
-      {/* <AuthRoutes /> */}
-      <AppRoutes />
+      {authenticated ? <AppRoutes /> : <AuthRoutes />}
     </BrowserRouter>
   )
 }
